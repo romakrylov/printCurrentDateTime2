81bce1217a53bcc5317391838f8d925ee044f8b5
Roman Krylov <0087897@gmail.com>
2022-09-12 18:54:04 +0300

    change program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.time.LocalDateTime;
+
 public class Main {
+    public static int i = 0;
+    public static int max = 10;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        iterate();
+    }
+
+    public static void iterate() {
+        if (i < max) {
+            i = i + 1;
+            System.out.println(i);
+            iterate();
+        }
     }
 }
